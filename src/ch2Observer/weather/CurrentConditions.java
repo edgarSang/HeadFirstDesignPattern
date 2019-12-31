c6e98191452c100b9d77d3e8bc83c630a74627db
edgarSang <poseidon0288@gmail.com>
2019-12-31 16:27:37 +0900

    pure observer pattern 완성

--- src/ch2Observer/weather/CurrentConditions.java
+++ src/ch2Observer/weather/CurrentConditions.java
@@ -2,19 +2,32 @@
 package ch2Observer.weather;
 
 import ch2Observer.Observer;
+import ch2Observer.Subject;
 
 public class CurrentConditions implements DisplayElements,Observer{
-
+	
+	//구지 생성자 맑도는 안쓰는데 왜 저장해야하지?, 나중에 탈퇴할때를 대비해서.
+	private Subject weatherData;
+	private float humidity;
+	private float temperature;
+	
+	
+	public CurrentConditions(Subject weatherData) {
+		this.weatherData = weatherData;
+		weatherData.registerObserver(this);
+		
+	}
+	
+	
 	@Override
 	public void display() {
-		// TODO Auto-generated method stub
-		
+		System.out.println(String.format("Current Conditions: temp: %d humidity: %d ", temperature, humidity));
 	}
 
 	@Override
 	public void update(float temp, float humidi, float pressure) {
-		// TODO Auto-generated method stub
-		
+		this.temperature = temp;
+		this.humidity = humidi;
 	}
 
 }
--- src/ch2Observer/weather/WeatherData.java
+++ src/ch2Observer/weather/WeatherData.java
@@ -1,40 +1,63 @@
 package ch2Observer.weather;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import ch2Observer.Observer;
 import ch2Observer.Subject;
 
 public class WeatherData implements Subject{
+	
+	private ArrayList<Observer> observers;
+	private float humidity;
+	private float temperature;
+	private float pressure;
+	private Random random;
+	
+	
+	public WeatherData() {
+		observers = new ArrayList<Observer>();
+	}
 
 	@Override
 	public void notifyObserver() {
-		
+		observers.stream().map(o -> o.update(temperature, humidity, pressure));
 	}
 	
-	void getTemp() {
-		
+	float getTemp() {
+		return random.nextFloat();
 	}
 	
-	void getHumidity() {
-		
+	float getHumidity() {
+		return random.nextFloat();
 	}
 	
-	void getPressure () {
-		
+	float getPressure () {
+		return random.nextFloat();
 	}
 
+	//가상 스테이션에서 갱신된 측정치를 받으면 옵저버들한테 알립니다.(왜 직접 notiy를 호출함안되지?)
 	void measurementsChanged() {
-		
+		notifyObserver();
+	}
+	
+	public void setMesuremetns() {
+		this.humidity = getHumidity();
+		this.pressure = getPressure();
+		this.temperature = getTemp();
+		measurementsChanged();
 	}
 
 	@Override
 	public void registerObserver(Observer o) {
-		// TODO Auto-generated method stub
-		
+		observers.add(o);
 	}
 
 	@Override
 	public void removeObserver(Observer o) {
-		// TODO Auto-generated method stub
-		
+		int i = observers.indexOf(o);
+		if(i > -1) {
+			observers.remove(i);
+		}
 	}
 }
